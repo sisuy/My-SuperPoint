ac21304c1e6351311135bbce82a940a28c74800a
sisuy <1808028940@qq.com>
2023-02-16 00:39:07 +1300

    Add: add batch normalisation in VGG encoder

--- model/modules/VGGBackbone.py
+++ model/modules/VGGBackbone.py
@@ -9,45 +9,53 @@ class VGGBackbone(torch.nn.Module):
 
         self.block1_1 = torch.nn.Sequential(
                 torch.nn.Conv2d(input_chanel,channels[0],kernel_size = 3,stride = 1,padding = 1),
-                torch.nn.ReLU(inplace=True)
+                torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[0])
                 )
 
         self.block1_2 = torch.nn.Sequential(
                 torch.nn.Conv2d(channels[0],channels[1],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[1]),
                 torch.nn.MaxPool2d(kernel_size=2,stride=2)
                 )
 
         self.block2_1 = torch.nn.Sequential(
                 torch.nn.Conv2d(channels[1],channels[2],kernel_size = 3,stride = 1,padding = 1),
-                torch.nn.ReLU(inplace=True)
+                torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[2]),
                 )
 
         self.block2_2 = torch.nn.Sequential(
                 torch.nn.Conv2d(channels[2],channels[3],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[3]),
                 torch.nn.MaxPool2d(kernel_size=2,stride=2)
                 )
 
         self.block3_1 = torch.nn.Sequential(
                 torch.nn.Conv2d(channels[3],channels[4],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[4]),
                 )
 
         self.block3_2 = torch.nn.Sequential(
                 torch.nn.Conv2d(channels[4],channels[5],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[5]),
                 torch.nn.MaxPool2d(kernel_size=2,stride=2)
                 )
 
         self.block4_1 = torch.nn.Sequential(
-                torch.nn.Conv2d(channels[3],channels[4],kernel_size = 3,stride = 1,padding = 1),
+                torch.nn.Conv2d(channels[5],channels[6],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[6]),
                 )
 
         self.block4_2 = torch.nn.Sequential(
-                torch.nn.Conv2d(channels[4],channels[5],kernel_size = 3,stride = 1,padding = 1),
+                torch.nn.Conv2d(channels[6],channels[7],kernel_size = 3,stride = 1,padding = 1),
                 torch.nn.ReLU(inplace=True),
+                torch.nn.BatchNorm2d(channels[7])
                 )
     def forward(self,input):
         out = self.block1_1(input)
